188ca02fa27f3cb8d6e85a96230c3ae768b6056f
AvihayR <avihayrabi@gmail.com>
2024-05-31 16:46:02 +0300

    Add next-guess logic according to user clicks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,42 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, Alert } from "react-native"
 import Title from "../components/ui/Title"
-import utils from "../utils"
+import { generateRandomBetween } from "../utils"
 import { useState } from "react"
 import NumberContainer from "../components/game/NumberContainer"
+import PrimaryButton from "../components/ui/PrimaryButton"
+
+
+let minNumber = 1
+let maxNumber = 100
 
 export default function GameScreen({ chosenNumber }) {
-    const initialGuess = utils.generateRandomBetween(1, 100, chosenNumber)
+    const initialGuess = generateRandomBetween(minNumber, maxNumber, chosenNumber)
     const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
+
+    function nextGuessHandler(direction) {
+        if ((direction === 'lower' && currentGuess < chosenNumber) ||
+            (direction === 'higher' && currentGuess > chosenNumber)) {
+            Alert.alert('Come on..', 'We both know that\'s not true 😉', [{ text: 'Sorry', style: 'cancel' }])
+            return
+        }
+        else if (direction === 'lower') {
+            maxNumber = currentGuess
+        } else {
+            minNumber = currentGuess + 1
+        }
+
+        const newRandomNumber = generateRandomBetween(minNumber, maxNumber, currentGuess)
+
+        if (newRandomNumber === chosenNumber) {
+            setCurrentGuess(newRandomNumber)
+            return console.log('Horray! 🎉')
+        }
+
+        setCurrentGuess(newRandomNumber)
+        console.log('min:', minNumber, 'Max:', maxNumber)
+    }
+
 
     return (
         <View style={styles.screen}>
@@ -15,8 +45,10 @@ export default function GameScreen({ chosenNumber }) {
 
             <View>
                 <Text>Higher or lower?</Text>
-                {/* + */}
-                {/* - */}
+                <View>
+                    <PrimaryButton onPress={() => { nextGuessHandler('higher') }}>+</PrimaryButton>
+                    <PrimaryButton onPress={() => { nextGuessHandler('lower') }}>-</PrimaryButton>
+                </View>
             </View>
             <View>
                 {/* LOG ROUNDS */}
--- utils.js
+++ utils.js
@@ -1,13 +1,12 @@
-const utils = {
-    generateRandomBetween: (min, max, exclude) => {
-        const rndNum = Math.floor(Math.random() * (max - min)) + min
 
-        if (rndNum === exclude) {
-            return generateRandomBetween(min, max, exclude)
-        } else {
-            return rndNum
-        }
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min
+
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude)
+    } else {
+        return rndNum
     }
 }
 
-export default utils
+export { generateRandomBetween }
