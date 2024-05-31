14aab25dff41050fd068846ef17234ad821d28db
AvihayR <avihayrabi@gmail.com>
2024-05-31 19:00:14 +0300

    Darken main 500 & 700 colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,8 +1,8 @@
 const Colors = {
     primary100: '#a61d66',
-    primary500: '#741c4d',
+    primary500: '#681040',
     primary600: '#5e0b39',
-    primary700: '#60173f',
+    primary700: '#560c35',
     primary800: '#4a0327',
     secondary500: '#ddb52d',
     secondary600: '#ddb52f',
