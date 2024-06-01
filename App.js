455d9a7fd209e4ab45c820fbb2e4352ab21eefd3
AvihayR <avihayrabi@gmail.com>
2024-06-01 17:34:14 +0300

    Make use of useFonts hook and Expo-SplashScreen

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
+import { useEffect, useState } from 'react'
 import { ImageBackground, StyleSheet, SafeAreaView, StatusBar } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from './screens/GameScreen'
 import diceBgImg from './assets/images/background.png'
-import { useState } from 'react'
 import Colors from './constants/colors'
 import GameOverScreen from './screens/GameOverScreen'
-
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
   const [pickedNumber, setPickedNumber] = useState(null)
   const [isGameOver, setGameIsOver] = useState(false)
-
   let screen = <StartGameScreen onPickNumber={setPickedNumber} />
+
+  const [isFontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+
+  // Prevent the splash screen from auto-hiding
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync()
+  }, [])
+
+  // Hide the splash screen when the fonts are loaded
+  useEffect(() => {
+    if (isFontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [isFontsLoaded])
 
   if (pickedNumber) {
     screen = <GameScreen chosenNumber={pickedNumber} onGameOver={gameOverHandler} />
